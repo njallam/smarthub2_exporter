ad6d08c6b8207122ecef957db641c0d59cd50ad8
Nicholas Allam <njallam@outlook.com>
2022-01-29 10:46:14 +0000

    Use custom registry to only collect hub metrics

--- main.go
+++ main.go
@@ -17,15 +17,16 @@ func main() {
 		ip = "192.168.1.254"
 	}
 
+	registry := prometheus.NewRegistry()
 	collector := newSmartHub2Collector(ip)
 
-	prometheus.MustRegister(collector)
+	registry.MustRegister(collector)
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte(`<html><head><title>Smart Hub 2 Exporter</title></head>` +
 			`<body><a href="metrics">Metrics</a></body></html>`))
 	})
-	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
 
 	log.Println("Beginning to serve on port :9906")
 	log.Fatal(http.ListenAndServe(":9906", nil))
